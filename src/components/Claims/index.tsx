4bc08a77e0e8afe0cf9b441edca165a7017a3ee4
Enzo3322 <enzo.spag14@gmail.com>
2022-03-21 12:09:16 -0300

    add validation route

--- src/components/Claims/index.tsx
+++ src/components/Claims/index.tsx
@@ -15,13 +15,24 @@ const ClaimsGrid: React.FC<ClaimProps> = ({ data, loading }) => (
 				{data.map((e, i) => {
 					return (
 						<div key={i}>
-							<p>Id: {e._id}</p>
-							<p>time: {e.created_at}</p>
-							<p>Campus: {e.campus}</p>
-							<p>Prioridade: {e.priority}</p>
-							<p>Coordenador: {e.coordinator}</p>
-							<p>Prioridade: {e.priority}</p>
-							<p>Descrição: {e.description}</p>
+							<p>
+								<b>Id:</b> {e._id}
+							</p>
+							<p>
+								<b>Time:</b> {e.created_at}
+							</p>
+							<p>
+								<b>Campus:</b> {e.campus}
+							</p>
+							<p>
+								<b>Coordenador:</b> {e.coordinator}
+							</p>
+							<p>
+								<b>Prioridade:</b> {e.priority}
+							</p>
+							<p>
+								<b>Descrição:</b> {e.description}
+							</p>
 						</div>
 					);
 				})}
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+var xApiKey = process.env.REACT_APP_X_API_KEY || '';
+
 const api = axios.create({
   baseURL: "https://unip-back.herokuapp.com/",
+  headers: {
+    "Content-Type": "application/json",
+    "not-api-key": xApiKey,
+  }
 });
 
 export default api;
